c250616e7e10cb7675fc6595eee724b4bb51258d
Peter-McKinney <45462076+Peter-McKinney@users.noreply.github.com>
2024-07-13 01:10:08 -0500

    Rework path parsing

--- open-file-nvim.sh
+++ open-file-nvim.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-escape_input() {
-    echo "$1" | sed 's/\\/\\\\/g; s/"/\\"/g'
-}
-
 if ! command -v fzf &> /dev/null
 then
     echo "fzf could not be found"
@@ -11,14 +7,25 @@ then
     exit 1
 fi
 
+remove_invalid_path_characters() {
+    echo "$1" | awk '{ gsub(/[^[:alnum:][:space:]._~\/-]/, ""); print }'
+}
+
 buffer_name="tmux_capture_buffer"
 
 tmux capture-pane -J -b "$buffer_name"
 captured_content=$(tmux show-buffer -b "$buffer_name")
-escaped_content=$(escape_input "$captured_content")
 
-files=$(echo "$escaped_content" | awk '{
-    while (match($0, /[^[:space:]("]*\/[[:alnum:]._-]+(\/[[:alnum:]._-]+)*\.[[:alnum:]]+/)) {
+# remove any characters that are not valid in a file path
+# remove http or ftp urls.
+sanitized_content=$(remove_invalid_path_characters "$captured_content")
+sanitized_content=$(echo "$sanitized_content" | sed 's/http[^ ]*//g')
+sanitized_content=$(echo "$sanitized_content" | sed 's/ftp[^ ]*//g');
+
+# awk for anything that resembles a file path with a file extension on the end.
+# only return unique paths
+files=$(echo "$sanitized_content" | awk '{
+    while (match($0, /[^[:space:]]*\/[[:alnum:]._-]+(\/[[:alnum:]._-]+)*\.[[:alnum:]]+/)) {
         path = substr($0, RSTART, RLENGTH)
         if (!seen[path]++) {
             print path
@@ -27,8 +34,13 @@ files=$(echo "$escaped_content" | awk '{
     }
 }')
 
+#handle ~ home expansion 
+files=$(echo "$files" | sed 's/~/\$HOME/g')
+
 if [ -z "$files" ]; then
   echo "No files found."
 else
   tmux split-window -h -c "#{pane_current_path}" "echo \"$files\" | fzf -m | xargs -I {} $EDITOR {}"
 fi
+
+tmux delete-buffer -b "$buffer_name"
