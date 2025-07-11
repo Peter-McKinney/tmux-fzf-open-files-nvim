898cc2f86d34cdef6004f3a79b7480a334ac53ca
Peter-McKinney <45462076+Peter-McKinney@users.noreply.github.com>
2025-07-11 12:57:23 -0500

    Fix up scripts

--- dev/link-plugin-locally.sh
+++ dev/link-plugin-locally.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # replace the github link for the local tmux-plugins link, commented out
-sed -i "s|set -g @plugin 'Peter-McKinney/tmux-fzf-open-files-nvim'|#set -g @plugin 'tmux-plugins/tmux-fzf-open-files-nvim'|" ~/.tmux.conf
+sed -i '' "s|set -g @plugin 'Peter-McKinney/tmux-fzf-open-files-nvim'|#set -g @plugin 'tmux-plugins/tmux-fzf-open-files-nvim'|" ~/.tmux.conf
 
 # uninstall using tpm from the plugin folder
 ~/.tmux/plugins/tpm/bin/clean_plugins
 
 # link this repository to the tmux plugins
-ln -s ~/github/tmux-fzf-open-files-nvim ~/.tmux/plugins/tmux-fzf-open-files-nvim
+ln -sf ~/github/tmux-fzf-open-files-nvim ~/.tmux/plugins/
 
 # uncomment local link
-sed -i "s|#set -g @plugin 'tmux-plugins/tmux-fzf-open-files-nvim'|set -g @plugin 'tmux-plugins/tmux-fzf-open-files-nvim'|" ~/.tmux.conf
+sed -i '' "s|#set -g @plugin 'tmux-plugins/tmux-fzf-open-files-nvim'|set -g @plugin 'tmux-plugins/tmux-fzf-open-files-nvim'|" ~/.tmux.conf
--- dev/restore-git-plugin.sh
+++ dev/restore-git-plugin.sh
@@ -3,7 +3,7 @@
 unlink ~/.tmux/plugins/tmux-fzf-open-files-nvim
 
 # replace the local link for the github link
-sed -i "s|set -g @plugin 'tmux-plugins/tmux-fzf-open-files-nvim'|set -g @plugin 'Peter-McKinney/tmux-fzf-open-files-nvim'|" ~/.tmux.conf
+sed -i '' "s|set -g @plugin 'tmux-plugins/tmux-fzf-open-files-nvim'|set -g @plugin 'Peter-McKinney/tmux-fzf-open-files-nvim'|" ~/.tmux.conf
 
 # install using tpm from the plugins folder
 ~/.tmux/plugins/tpm/bin/install_plugins
